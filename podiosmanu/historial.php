660ce92ffbea907512b2bc1b02a259f338eea198
manu10000189 <43579040+manu10000189@users.noreply.github.com>
2019-03-14 17:38:52 +0100

    Add files via upload

---
+++ podiosmanu/historial.php
@@ -0,0 +1,7 @@
+<div class="filtromejores">
+			<form action="filtroadmon.proc.php">
+  <input type="radio" name="filtro" value="10mejores"> Los 10 mejores<br>
+  <input type="radio" name="filtro" value="mes"> Los mejores del mes<br>
+  <input class="btn" type="submit" name="submit">
+</form>
+</div>
