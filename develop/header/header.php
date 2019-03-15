7cdb961e4b5c369dfa6a224843642e8fbb9f362d
DaniAlvrez <43578983+DaniAlvrez@users.noreply.github.com>
2019-03-15 17:35:47 +0100

    Estilos, header y footer+ pagina inicio
    estilos de las paginas, footer y header

---
+++ develop/header/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	 	<link href="header.css" rel="stylesheet" type="text/css"/>
+	 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"/>
+	</head>
+	<body >
+		<div class="header">
+			<img class="imagen" src="logohead.png">
+			<nav id="menu">
+				<ul>
+	 				<li><a href="#"><img  src="idioma.png" height="35px"><br>Español<i class="fas fa-caret-down"></i></a>
+		    			<ul>
+		      				<li><a href="#">Inglés</a></li>
+		      				<li><a href="#">Alemán</a></li>
+					    </ul>
+	  				</li>
+					<li><a href="#"><img src="points.png" height="35px"><br><font color="#88c33d">0 EcoPoints</font></a></li>
+	  				<li><a href="#"><div class="icons" align="center"><img src="perfil.png"><br>Perfil<i class="fas fa-caret-down"></i></div></a>
+	    				<ul>
+							<li><a href="#">Ajustes de Perfil</a></li>
+							<li><a href="#">Mi actividad</a></li>
+							<li><a href="#">Cerrar Sesión</a></li>
+						</ul>
+	  				</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="navegacion">
+			<a href="#" ><img class="iconav" src="home.png"></a>
+			<a href="#">Home</a>
+			<a href="#"><img class="iconav2" src="ranking.png"></a>
+			<a href="#">Ranking</a>
+			<a href="#"><img class="iconav2" src="donaciones.png"></a>
+			<a href="#">Donaciones</a>
+			
+		</div>
+	</body>
+</html>
