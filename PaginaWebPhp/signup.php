eb58efab0f667954a5a3e9ea967a39516df9fc05
kalel1500 <100005989.joan23@fje.edu>
2019-03-25 20:36:50 +0100

    registro con estilos funcional - no validado

--- PaginaWebPhp/signup.php
+++ PaginaWebPhp/signup.php
@@ -9,11 +9,11 @@
 		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			//$msg .= 'El correo ES valido';
 			//SELECT nombre, email FROM tbl_usuarios WHERE nombre=usu2 or email=usu1@mail.com
-			if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
-				if ($_POST['password'] == $_POST['confirm_password']) {
+			if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['repeatedPassword'])) {
+				if ($_POST['password'] == $_POST['repeatedPassword']) {
 					
 					$records = $conn->prepare('SELECT usuario_usuario FROM tbl_usuario WHERE usuario_usuario=:usuario');
-					$records->bindParam(':usuario', $_POST['usuario']);
+					$records->bindParam(':usuario', $_POST['username']);
 					$records->execute();
 					$resultsUsu = $records->fetch(PDO::FETCH_ASSOC);
 
@@ -32,11 +32,9 @@
 					}
 
 					if ($pass) {
-						$sql = "INSERT INTO `tbl_usuario` (`usuario_usuario`, `usuario_nombre`, `usuario_apellido`, `usuario_email`, `usuario_contrasena`, `usuario_ultimaConexion`, `usuario_ultimoReset`, `usuario_fondosTotales`, `usuario_fondosMensuales`, `grupo_id`, `nivel_id`, `foto_id`) VALUES (:usuario, :nombre, :apellido, :email, :password, NULL, NULL, 0, 0, (SELECT `grupo_id` FROM `tbl_grupo` WHERE `grupo_nombre` = 'Usuario'), (SELECT `nivel_id` FROM `tbl_nivel` WHERE `nivel_numero` = 1), (SELECT `foto_id` FROM `tbl_foto` WHERE `foto_nombre` = '1_foto01'))";
+						$sql = "INSERT INTO `tbl_usuario` (`usuario_usuario`, `usuario_nombre`, `usuario_apellido`, `usuario_email`, `usuario_contrasena`, `usuario_ultimaConexion`, `usuario_ultimoReset`, `usuario_fondosTotales`, `usuario_fondosMensuales`, `grupo_id`, `nivel_id`, `foto_id`) VALUES (:usuario, NULL, NULL, :email, :password, NULL, NULL, 0, 0, (SELECT `grupo_id` FROM `tbl_grupo` WHERE `grupo_nombre` = 'Usuario'), (SELECT `nivel_id` FROM `tbl_nivel` WHERE `nivel_numero` = 1), (SELECT `foto_id` FROM `tbl_foto` WHERE `foto_nombre` = '1_foto01'))";
 						$stmt = $conn->prepare($sql);
-						$stmt->bindParam(':usuario', $_POST['usuario']);
-						$stmt->bindParam(':nombre', $_POST['nombre']);
-						$stmt->bindParam(':apellido', $_POST['apellido']);
+						$stmt->bindParam(':usuario', $_POST['username']);
 						$stmt->bindParam(':email', $_POST['email']);
 						$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 						$stmt->bindParam(':password', $password);
@@ -46,7 +44,7 @@
 							$km = 0;
 							$fechaInicio = time();
 							$fechaIFin = time()+1;
-							$usu = $_POST['usuario'];
+							$usu = $_POST['username'];
 							$query = "INSERT INTO `tbl_historial` (`historial_fechaInicio`, `historial_fechaFin`, `historial_kilometros`, `usuario_id`, `bicicleta_id`) VALUES ('$fechaInicio', '$fechaIFin', $km, (SELECT `usuario_id` FROM `tbl_usuario` WHERE `usuario_usuario` = '$usu'), 1);";
 							$database->query($query);
 							echo $query;
@@ -73,84 +71,59 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>EcoCycling | Registro</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/styles.css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>EcoCycling</title>
+		<link rel="stylesheet" href="assets/archivosSignup/assets/css/bootstrap.css">
+		<link rel="stylesheet" href="assets/archivosSignup/assets/css/styles.css">
+	</head>
 
-<body>
-	<div class="bg"></div>
-	<div class="container text-center">
-		<h1 class="mt-5 text-dark font-weight-light">Únete hoy, practica el #<span class="green-text">Eco</span>Cycling</h1>
-		<p class="lead text-dark-50">An attractive yet subtle dropdown animation for dropdown menus loacated within a
-			Bootstrap navbar</p>
-	</div>
-	<div class="container-fluid">
-		<div class="row justify-content-center align-self-center">
-			<section class="col-sm-10 col-md-4">
-				<div class="contenedor-registro">
-					<form action="" class="main-form needs-validation" novalidate>
-						<!-- <div class="row">
-								<div class="col">
-									<div class="form-group">
-										<label for="firstname">Nombre</label>
-										<input type="text" name="firstname" id="firstname" class="form-control" />
-									</div>
-								</div>
-								<div class="col">
-									<div class="form-group">
-										<label for="lastname">Apellidos</label>
-										<input type="text" name="lastname" id="lastname" class="form-control">
-									</div>
-								</div>
-							</div> -->
-						<div class="form-group">
-							<label for="username">Usuario *</label>
-							<input type="text" name="username" id="username" class="form-control">
-						</div>
-						<div class="form-group">
-							<label for="email">Correo electrónico *</label>
-							<input type="text" name="email" id="email" class="form-control">
-							<small class="form-text text-muted">
-								ejemplo@ejemplomail.com
-							</small>
-						</div>
-						<div class="form-group">
-							<label for="password">Contraseña *</label>
-							<input type="password" name="password" id="password" class="form-control">
-						</div>
-						<div class="form-group">
-							<label for="password">Repetir Contraseña *</label>
-							<input type="password" name="repeatedPassword" id="repeatedPassword" class="form-control">
-						</div>
-						<!-- <div class="form-group">
-								<label for="gender">Género</label>
-								<select name="gender" id="gender" class="form-control">
-									<option value="male">Hombre</option>
-									<option value="female">Mujer</option>
-									<option value="female">Prefiero no decirlo</option>
-								</select>
-							</div> -->
-						<div class="form-check">
-							<input type="checkbox" id="accept-terms" class="form-check-input">
-							<label for="accept-terms" class="form-check-label">Acepto los términos y condiciones</label>
-						</div><br>
-						<button type="submit" class="btn btn-success btn-md btn-block">Submit</button>
-					</form>
-				</div>
-			</section>
+	<body>
+		<div class="container text-center" style="margin-top: 20px; margin-bottom: 20px;">
+			<h1 class="text-dark font-weight-light">Únete hoy, practica el #<span class="green-text">Eco</span>Cycling</h1>
 		</div>
-	</div>
-	</section>
 
+		<section class="container">
+			<div class="row justify-content-center align-self-center">
+				<div class="col-sm-10 col-md-4">
 
-</body>
-<script src="assets/js/jquery-3.3.1.js"></script>
-<script src="assets/js/bootstrap.bundle.js"></script>
-<script src="assets/js/bootstrap.js"></script>
+					<?php if (!empty($msg)) { ?>
+						<div class="alert alert-danger">
+							<?php echo $msg; ?>
+						</div>
+					<?php } ?>
+
+					<div class="contenedor-registro">
+						<form action="signup.php" class="main-form needs-validation" method="POST" novalidate>
+							<div class="form-group">
+								<label for="username">Usuario *</label>
+								<input type="text" name="username" id="username" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="email">Correo electrónico *</label>
+								<input type="text" name="email" id="email" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="password">Contraseña *</label>
+								<input type="password" name="password" id="password" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="password">Repetir Contraseña *</label>
+								<input type="password" name="repeatedPassword" id="repeatedPassword" class="form-control">
+							</div>
+							<br>
+							<button type="submit" class="btn btn-success btn-md btn-block">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+
+	<script src="assets/archivosSignup/assets/js/jquery-3.3.1.js"></script>
+	<script src="assets/archivosSignup/assets/js/bootstrap.bundle.js"></script>
+	<script src="assets/archivosSignup/assets/js/bootstrap.js"></script>
 
 </html>
