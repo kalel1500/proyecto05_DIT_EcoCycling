14c7ff7324a182620f0c84053091e9a71140a9d8
kalel1500 <100005989.joan23@fje.edu>
2019-03-28 16:05:48 +0100

    imagenes dondaciones

--- PaginaWebPhp/donations.php
+++ PaginaWebPhp/donations.php
@@ -8,7 +8,13 @@
 	$money = $database->query("SELECT usuario_fondosTotales FROM tbl_usuario WHERE usuario_id={$_SESSION['ecocycling_user_id']};");
 	while ($row = $money->fetch_assoc()) {
 		$points = round($row['usuario_fondosTotales']);
-	} ?>
+
+		$pay ($points == 0) ? false : true;
+	}
+
+
+
+?>
 
 
 
@@ -22,7 +28,7 @@
 				<div class="row" style="border:1px solid #28a745;border-radius:5px;">
 						<div class="col-sm-6 col-md-6">
 								<div class="thumbnail">
-										<img src="assets/img/<?=$photo?>.jpg" alt="...">
+										<img src="assets/img/org/<?=$photo?>.jpg" alt="...">
 											<div class="caption text-center">
 												<h3><?=$row['entidad_nombre']?></h3>
 												<p>Dinero enviado: <?=$row['entidad_dineroActual']?> €</p>
@@ -38,8 +44,8 @@
 											<h3>Donar a <?=$row['entidad_nombre']?></h3>
 											<br><br><p><?=$row['entidad_descripcion']?></p><br>
 											<p>Cartera: <?=$points?> ECOPoints</p>
-											Donar: <input type="text" name="donation" id="donation" size="4"/> ECOPoints<br><br><br><br>
-											<p><a href="#" class="btn btn-success" role="button">Realizar donación</a></p>
+											Donar: <input form="form-<?=$photo?>" type="text" name="donation" id="donation" size="4" <?php if(!($pay)) {echo "disabled";} ?> /> ECOPoints<br><br><br><br>
+											<p><button type="submit" href="#" form="form-<?=$photo?>" class="btn btn-success" role="button">Realizar donación</button></p>
 										</div>
 										<form id="form-<?=$photo?>" action="index.html" method="post">
 
